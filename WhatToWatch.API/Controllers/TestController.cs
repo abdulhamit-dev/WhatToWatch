31e3434af0b6c5b1926ec181fb2d335f56a51520
abdulhamit-dev <hmd62392@gmail.com>
2022-11-03 16:47:18 +0300

    Test branch

---
+++ WhatToWatch.API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhatToWatch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
