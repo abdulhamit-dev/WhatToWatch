6aa9acbdf086d100d2cd928b0e2bd3138224ccfb
abdulhamit-dev <hmd62392@gmail.com>
2022-11-03 17:02:32 +0300

    Test bracnh first Get endpoint

---
+++ WhatToWatch.API/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhatToWatch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public ActionResult Get() =>Ok("success");
+    }
+}
